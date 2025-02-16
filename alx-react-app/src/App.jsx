599e4a7caf144bfc1bd4554b89ac6e7d3c4f55d7
elDapper <elormjerry64@gmail.com>
2025-02-16 17:01:11 +0000

    created components in app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -29,6 +32,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
         Click on the Vite and React logos to learn more
       </p>
     </>
