9e8e81e63d671fe728fb38ddef59a48a8f46c9ca
elDapper <elormjerry64@gmail.com>
2025-02-23 20:52:16 +0000

    tweaked company files

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,33 +1,26 @@
-
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
 import Home from './Home';
-import Contact from './Contact';
 import About from './About';
 import Services from './Services';
-import  './App.css'
+import Contact from './Contact';
+import Footer from './Footer';
 
 function App() {
   return (
     <>
-          <BrowserRouter >
-        <Routes >
-          <Route path='/'Home={<Home />} />
-          <Route path='/'About={ <About />} />
-          <Route path='/'Contact={<Contact />}/>
-          <Route path='/'Services={<Services />} />
-        </Routes >
-      </BrowserRouter>
-
-      
-      <About/>
-      <Home />
-      <Contact/>
-      <Services/>
-     
-
-      
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
     </>
+
   );
 }
 
--- my-company/src/Navbar.jsx
+++ my-company/src/Navbar.jsx
@@ -1,23 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-function Navbar () {
-
-    return (
-        <nav style={{
-        
-            color:'white',
-            padding: '10px',
-            textAlign:'center'
-        }}>
-            <Link to="/">Home</Link>
-            <Link to="./src/About">About</Link>
-            <Link to="./src/Services">Services</Link>
-            <Link to ="./src/Contact">Contact</Link>
-        </nav>
-    );
-
+function Navbar() {
+  return (
+    <nav style={{ backgroundColor: '#333', padding: '10px', color: '#fff' }}>
+      <Link to="/" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Contact</Link>
+    </nav>
+  );
 }
-
-
 
 export default Navbar;
