cdad266387f53c8fd4f83ddc07dfd12bc5a3f1a9
elDapper <elormjerry64@gmail.com>
2025-03-06 09:50:03 +0000

    ....

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,12 +4,32 @@ const useRecipeStore = create((set, get) => ({
   recipes: [],
   favorites: [],
   recommendations: [],
+  // Add a new recipe
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  // Delete a recipe by ID
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
+  // Update an existing recipe
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+  // Set the entire recipes list (initialize or replace)
+  setRecipes: (recipes) => set({ recipes }),
+  // Add a recipe to favorites
   addFavorite: (recipeId) =>
     set((state) => ({ favorites: [...state.favorites, recipeId] })),
+  // Remove a recipe from favorites
   removeFavorite: (recipeId) =>
     set((state) => ({
       favorites: state.favorites.filter((id) => id !== recipeId),
     })),
+  // Generate recommendations based on favorites
   generateRecommendations: () => {
     const { recipes, favorites } = get();
     const recommended = recipes.filter(
@@ -17,18 +37,6 @@ const useRecipeStore = create((set, get) => ({
     );
     set({ recommendations: recommended });
   },
-  addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (recipeId) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
-    })),
-  updateRecipe: (updatedRecipe) =>
-    set((state) => ({
-      recipes: state.recipes.map((recipe) =>
-        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
-      ),
-    })),
 }));
 
 export default useRecipeStore;
