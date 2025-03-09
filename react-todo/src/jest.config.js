fa8e77388076285eba95dcee6ddc83100be296bf
Jeremiah Elorm Aleawobu <67351384+eL-Dapper-Jhunior@users.noreply.github.com>
2025-03-09 22:34:56 +0000

    Delete react-todo directory

--- react-todo/src/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testEnvironment: "jsdom", // Use jsdom for React testing
-    transform: {
-      "^.+\\.jsx?$": "babel-jest", // Use babel-jest to transform JSX
-    },
-    moduleNameMapper: {
-      "\\.(css|less|scss|sass)$": "identity-obj-proxy", // Mock CSS imports
-    },
-  };
