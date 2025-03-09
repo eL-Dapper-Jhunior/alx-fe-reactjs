4fe98cd684439b39c59d4e47285feb13bb7aa66d
elDapper <elormjerry64@gmail.com>
2025-03-09 19:48:06 +0000

    initial commit

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchPosts = async () => {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return response.data;
+};
+
+const PostsComponent = () => {
+  const { data: posts, error, isLoading, refetch } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching posts!</p>;
+
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refresh Posts</button>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
