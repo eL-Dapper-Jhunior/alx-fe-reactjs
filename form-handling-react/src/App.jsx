f6ed0b35bf01e09102ab732c3048fe3d0084f473
elDapper <elormjerry64@gmail.com>
2025-03-09 15:21:21 +0000

    initial commmit

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RegistrationForm from './components/RegistrationForm'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+        <h1>User Registratiion</h1>
+        <RegistrationForm />
+
+        <h1>User Registration</h1>
+      <FormikForm />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
