124c73116260b0d9137d459a0877e40cd45c0e52
elDapper <elormjerry64@gmail.com>
2025-03-16 13:41:39 +0000

    move tail_files

--- tailwind-react-integration/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Paths to scan for used styles
-    theme: { extend: {} }, // Extend Tailwind's default styles if needed
-    plugins: [], // Add extra Tailwind plugins here if needed
-  };
-  
